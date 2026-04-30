14f49de0818346cf147f61b316956bf7e29957d8
网上邻居 <408135242@qq.com>
2026-04-30 17:28:19 +0800

    add auto-import for classnames and update dependencies

--- apps/one-map-web/vite.config.ts
+++ apps/one-map-web/vite.config.ts
@@ -2,9 +2,21 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 import path from 'path';
+import autoImport from 'unplugin-auto-import/vite';
 
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    autoImport({
+      imports: [
+        {
+          classnames: [['default', 'cn']],
+        },
+      ],
+      dts: 'src/auto-imports.d.ts',
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
