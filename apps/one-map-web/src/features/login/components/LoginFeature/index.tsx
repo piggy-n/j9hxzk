3d54a322dfd85f8d61436356c3603255b47b0967
网上邻居 <408135242@qq.com>
2026-05-09 16:49:15 +0800

    feat: add animated scan light effect and improve login feature styles

--- apps/one-map-web/src/features/login/components/LoginFeature/index.tsx
+++ apps/one-map-web/src/features/login/components/LoginFeature/index.tsx
@@ -1,25 +1,65 @@
+import { Lock, User } from '@element-plus/icons-vue';
+import { ElButton, ElCheckbox, ElForm, ElFormItem, ElInput } from 'element-plus';
+import 'element-plus/es/components/button/style/css';
+import 'element-plus/es/components/checkbox/style/css';
+import 'element-plus/es/components/form-item/style/css';
+import 'element-plus/es/components/form/style/css';
+import 'element-plus/es/components/input/style/css';
+
 import styles from './style.module.scss';
 
 export default defineComponent({
   name: 'LoginFeature',
   setup() {
+    const form = reactive({
+      account: '',
+      password: '',
+      rememberAccount: true,
+      rememberPassword: true,
+    });
+
     return () => (
       <section class={styles.loginFeature}>
         <header class={styles.header}>
           <h1>调查监测现状图</h1>
           <p>WELCOME!</p>
         </header>
-        <div class={styles.body}>
-          <div class={styles.field} />
-          <div class={styles.field} />
+        <ElForm class={styles.form} labelPosition="top" model={form}>
+          <ElFormItem class={styles.formItem}>
+            <ElInput
+              class={styles.input}
+              clearable
+              placeholder="请输入账号"
+              prefix-icon={User}
+              size="large"
+              v-model={form.account}
+            />
+          </ElFormItem>
+          <ElFormItem class={styles.formItem}>
+            <ElInput
+              class={styles.input}
+              placeholder="请输入密码"
+              prefix-icon={Lock}
+              show-password
+              size="large"
+              type="password"
+              v-model={form.password}
+            />
+          </ElFormItem>
           <div class={styles.options}>
-            <span class={styles.option} />
-            <span class={styles.option} />
+            <ElCheckbox class={styles.checkbox} v-model={form.rememberAccount}>
+              记住账号
+            </ElCheckbox>
+            <ElCheckbox class={styles.checkbox} v-model={form.rememberPassword}>
+              记住密码
+            </ElCheckbox>
           </div>
           <div class={styles.actions}>
-            <div class={styles.button} />
+            <ElButton class={styles.submitButton} nativeType="button" size="large" type="primary">
+              <span class={styles.submitLabel}>登录</span>
+            </ElButton>
           </div>
-        </div>
+        </ElForm>
       </section>
     );
   },
